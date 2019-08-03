91cd8377a3db10108f2cd15472294a4cd2aa4bd1
Akash Pandey <akskipadhai@gmail.com>
2019-08-03 22:42:26 +0530

    Conceptualize button element as react component and add minus button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 const Display = ({ counter }) => <div> {counter} </div> //arrow function version and destructured prop.
-
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 const App = props => {
   const [counter, setCounter] = useState(0);
   const setToValue = (value) => setCounter(value)
@@ -9,13 +13,11 @@ const App = props => {
   return (
   <div>
   <Display counter={counter}/>
-  <button onClick={() => setToValue(counter+1)}>
-    plus
-  </button>
-  <button onClick={() => setToValue(0)}>
-    zero</button>
+  <Button onClick={() => setToValue(counter+1)} text='plus'/>
+  <Button onClick={() => setToValue(counter-1)} text='minus'/>
+  <Button onClick={() => setToValue(0)} text='zero'/>
   </div>
-    )
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
